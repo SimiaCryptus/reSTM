a9c69d11c2a64483bcbf50790a768aa1a39ce017
Andrew Charneski <acharneski@gmail.com>
2016-12-01 08:43:29 -0800

    SimpleSet fixes

---
+++ app/stm/TreeSet.scala
@@ -0,0 +1,172 @@
+package stm
+
+import storage.Restm
+import storage.Restm.PointerType
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, ExecutionContext, Future}
+
+
+object TreeSet {
+  def empty[T <: Comparable[T]] = new STMTxn[TreeSet[T]] {
+    override def txnLogic()(implicit ctx: STMTxnCtx, executionContext: ExecutionContext): Future[TreeSet[T]] = create[T]
+  }
+  def create[T <: Comparable[T]](implicit ctx: STMTxnCtx, executionContext: ExecutionContext) = STMPtr.dynamic[Option[BinaryTreeNode[T]]](None).map(new TreeSet(_))
+
+  def static[T <: Comparable[T]](id:PointerType) = new TreeSet(STMPtr.static[Option[BinaryTreeNode[T]]](id, None))
+}
+
+class TreeSet[T <: Comparable[T]](rootPtr : STMPtr[Option[BinaryTreeNode[T]]]) {
+  class AtomicApi()(implicit cluster: Restm, executionContext: ExecutionContext) {
+
+    class SyncApi(duration:Duration) {
+      def add(value: T) = Await.result(AtomicApi.this.add(value), duration)
+      def remove(value: T) = Await.result(AtomicApi.this.remove(value), duration)
+      def contains(value: T) = Await.result(AtomicApi.this.contains(value), duration)
+    }
+    def sync(duration:Duration) = new SyncApi(duration)
+    def sync = new SyncApi(10.seconds)
+
+    def add(value: T) = new STMTxn[Unit] {
+      override def txnLogic()(implicit ctx: STMTxnCtx, executionContext: ExecutionContext): Future[Unit] =
+        TreeSet.this.add(value).map(_=>Unit)
+    }.txnRun(cluster)(executionContext)
+
+    def remove(value: T) = new STMTxn[Unit] {
+      override def txnLogic()(implicit ctx: STMTxnCtx, executionContext: ExecutionContext): Future[Unit] =
+        TreeSet.this.remove(value).map(_=>Unit)
+    }.txnRun(cluster)(executionContext)
+
+    def contains(value: T) = new STMTxn[Boolean] {
+      override def txnLogic()(implicit ctx: STMTxnCtx, executionContext: ExecutionContext): Future[Boolean] =
+        TreeSet.this.contains(value)
+    }.txnRun(cluster)(executionContext)
+  }
+  def atomic(implicit cluster: Restm, executionContext: ExecutionContext) = new AtomicApi
+
+  def add(value: T)(implicit ctx: STMTxnCtx, executionContext: ExecutionContext) = {
+    rootPtr.readOpt().map(_.flatMap(x => x)).map(prev => {
+      prev.map(r => r += value).getOrElse(new BinaryTreeNode[T](value))
+    }).flatMap(newRootData=>rootPtr.write(Option(newRootData)))
+  }
+  def remove(value: T)(implicit ctx: STMTxnCtx, executionContext: ExecutionContext) = {
+    rootPtr.readOpt().map(_.flatMap(x => x)).map(prev => {
+      prev.flatMap(r => r -= value)
+    }).flatMap(newRootData=>rootPtr.write(newRootData))
+  }
+  def contains(value: T)(implicit ctx: STMTxnCtx, executionContext: ExecutionContext) = {
+    rootPtr.readOpt().map(_.flatMap(x => x)).map(prev => {
+      prev.map(_.contains(value)).getOrElse(false)
+    })
+  }
+  def min(value: T)(implicit ctx: STMTxnCtx, executionContext: ExecutionContext) = {
+    rootPtr.readOpt().map(_.flatMap(x => x)).map(prev => {
+      prev.map(_.min()).getOrElse(None)
+    })
+  }
+  def max(value: T)(implicit ctx: STMTxnCtx, executionContext: ExecutionContext) = {
+    rootPtr.readOpt().map(_.flatMap(x => x)).map(prev => {
+      prev.map(_.max()).getOrElse(None)
+    })
+  }
+}
+
+private case class BinaryTreeNode[T <: Comparable[T]]
+(
+  value: T,
+  left: Option[STMPtr[BinaryTreeNode[T]]] = None,
+  right: Option[STMPtr[BinaryTreeNode[T]]] = None
+) {
+
+  def min()(implicit ctx: STMTxnCtx, executionContext: ExecutionContext): T = {
+    right.map(_.sync.get.min).getOrElse(value)
+  }
+
+  def max()(implicit ctx: STMTxnCtx, executionContext: ExecutionContext): T = {
+    left.map(_.sync.get.min).getOrElse(value)
+  }
+
+  def -=(newValue: T)(implicit ctx: STMTxnCtx, executionContext: ExecutionContext): Option[BinaryTreeNode[T]] = {
+    val compare: Int = value.compareTo(newValue)
+    if (compare == 0) {
+      if(left.isEmpty && right.isEmpty) {
+        None
+      } else if(left.isDefined) {
+        left.map(leftPtr => {
+          val prevNode: BinaryTreeNode[T] = leftPtr.sync.get
+          val newValue: T = prevNode.min()
+          val maybeNode: Option[BinaryTreeNode[T]] = prevNode -= newValue
+          maybeNode.map(newNode=>{
+            leftPtr.sync <= newNode
+            BinaryTreeNode.this.copy(value=newValue)
+          }).getOrElse(BinaryTreeNode.this.copy(left=None, value=newValue))
+        })
+      } else {
+        right.map(rightPtr => {
+          val prevNode: BinaryTreeNode[T] = rightPtr.sync.get
+          val newValue: T = prevNode.max()
+          val maybeNode: Option[BinaryTreeNode[T]] = prevNode -= newValue
+          maybeNode.map(newNode=>{
+            rightPtr.sync <= newNode
+            BinaryTreeNode.this.copy(value=newValue)
+          }).getOrElse(BinaryTreeNode.this.copy(right=None, value=newValue))
+        })
+      }
+    } else if (compare < 0) {
+      left.map(leftPtr => {
+        Option((leftPtr.sync.get -= newValue).map(newLeft=>{
+          leftPtr.sync <= newLeft
+          BinaryTreeNode.this
+        }).getOrElse(BinaryTreeNode.this.copy(left=None)))
+      }).getOrElse({
+        throw new NoSuchElementException
+      })
+    } else {
+      right.map(rightPtr => {
+        Option((rightPtr.sync.get -= newValue).map(newRight=>{
+          rightPtr.sync <= newRight
+          BinaryTreeNode.this
+        }).getOrElse(BinaryTreeNode.this.copy(right=None)))
+      }).getOrElse({
+        throw new NoSuchElementException
+      })
+    }
+  }
+
+  def +=(newValue: T)(implicit ctx: STMTxnCtx, executionContext: ExecutionContext): BinaryTreeNode[T] = {
+    if (value.compareTo(newValue) < 0) {
+      left.map(leftPtr => {
+        leftPtr.sync <= (leftPtr.sync.get += newValue)
+        BinaryTreeNode.this
+      }).getOrElse({
+        this.copy(left = Option(STMPtr.dynamicSync(BinaryTreeNode(newValue))))
+      })
+    } else {
+      right.map(rightPtr => {
+        rightPtr.sync <= (rightPtr.sync.get += newValue)
+        BinaryTreeNode.this
+      }).getOrElse({
+        this.copy(right = Option(STMPtr.dynamicSync(BinaryTreeNode(newValue))))
+      })
+    }
+  }
+
+  def contains(newValue: T)(implicit ctx: STMTxnCtx, executionContext: ExecutionContext): Boolean = {
+    if (value.compareTo(newValue) == 0) {
+      true
+    } else if (value.compareTo(newValue) < 0) {
+      left.exists(_.sync.get.contains(newValue))
+    } else {
+      right.exists(_.sync.get.contains(newValue))
+    }
+  }
+
+  private def equalityFields = List(value, left, right)
+
+  override def hashCode(): Int = equalityFields.hashCode()
+
+  override def equals(obj: scala.Any): Boolean = obj match {
+    case x: BinaryTreeNode[_] => x.equalityFields == equalityFields
+    case _ => false
+  }
+}
---
+++ test/StmCollectionsSpecBase.scala
@@ -0,0 +1,79 @@
+import java.util.UUID
+import java.util.concurrent.Executors
+
+import org.scalatest.{BeforeAndAfterEach, MustMatchers, WordSpec}
+import org.scalatestplus.play.OneServerPerTest
+import stm._
+import storage.Restm._
+import storage.util._
+import storage.{RestmActors, _}
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutor, Future}
+
+abstract class StmCollectionsSpecBase extends WordSpec with MustMatchers {
+  implicit def cluster: Restm
+
+  implicit val executionContext = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
+  def randomUUIDs: Stream[String] = Stream.continually(UUID.randomUUID().toString.take(8))
+
+  "TreeSet" should {
+
+    "support basic (concurrent) operations" in {
+      val collection = TreeSet.static[String](new PointerType("test/SimpleTest"))
+      // Bootstrap collection synchronously to control contention
+      for (item <- randomUUIDs.take(5)) {
+        collection.atomic.sync.contains(item) mustBe false
+        collection.atomic.sync.add(item)
+        collection.atomic.sync.contains(item) mustBe true
+      }
+      // Run concurrent add/delete tests
+      val futures = for (item <- randomUUIDs.take(50)) yield Future {
+        try {
+          for(i <- 0 until 2) {
+            collection.atomic.sync.contains(item) mustBe false
+            collection.atomic.sync.add(item)
+            collection.atomic.sync.contains(item) mustBe true
+            collection.atomic.sync.remove(item)
+            collection.atomic.sync.contains(item) mustBe false
+          }
+        } catch {
+          case e => throw new RuntimeException(s"Error in item $item",e)
+        }
+      }
+      Await.result(Future.sequence(futures), 1.minutes)
+    }
+
+  }
+}
+
+class LocalStmCollectionsSpec extends StmCollectionsSpecBase with BeforeAndAfterEach {
+  override def beforeEach() {
+    cluster.internal.asInstanceOf[RestmActors].clear()
+  }
+
+  val cluster = LocalRestmDb
+}
+
+class LocalClusterStmCollectionsSpec extends StmCollectionsSpecBase with BeforeAndAfterEach {
+  private val pool: ExecutionContextExecutor = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
+  val shards = (0 until 8).map(_ => new RestmActors()(pool)).toList
+
+  override def beforeEach() {
+    shards.foreach(_.clear())
+  }
+
+  val cluster = new RestmCluster(shards)(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))
+}
+
+class IntegrationStmCollectionsSpec extends StmCollectionsSpecBase with OneServerPerTest {
+  val cluster = new RestmProxy(s"http://localhost:$port")(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))
+}
+
+class IntegrationInteralStmCollectionsSpec extends StmCollectionsSpecBase with OneServerPerTest {
+  private val newExeCtx: ExecutionContextExecutor = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
+  val cluster = new RestmImpl(new InternalRestmProxy(s"http://localhost:$port")(newExeCtx))(newExeCtx)
+}
+
+
+
