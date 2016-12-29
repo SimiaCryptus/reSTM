0a511059f67649401ea5ef0423698c098bf16a03
Andrew Charneski <acharneski@gmail.com>
2016-12-29 09:29:48 -0800

    Implemented forest cover classification test/demo

--- test/ClassificationTreeTestBase.scala
+++ test/ClassificationTreeTestBase.scala
@@ -1,3 +1,4 @@
+import java.io.FileInputStream
 import java.util.Date
 import java.util.concurrent.Executors
 
@@ -17,6 +18,7 @@ import storage.remote.{RestmCluster, RestmHttpClient}
 import storage.types.JacksonValue
 
 import scala.collection.JavaConverters._
+import scala.collection.immutable.Seq
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext}
 import scala.util.Random
@@ -129,6 +131,78 @@ abstract class ClassificationTreeTestBase extends WordSpec with MustMatchers wit
           println(s"Tree Path: "+JacksonValue.simple(collection.atomic().sync.getClusterPath(id)))
           println(s"Node Counts: "+JacksonValue.simple(collection.atomic().sync.getClusterCount(id)))
           println(s"Closest Inserted Words: "+closest)
+          println()
+        })
+
+        println(JacksonValue.simple(Util.getMetrics()).pretty)
+        Util.clearMetrics()
+      }
+    })
+    List(1000).foreach(items=> {
+      s"modeling on $items items from forest cover" in {
+        StmDaemons.start()
+        StmExecutionQueue.registerDaemons(8)
+        val collection = new ClassificationTree(new PointerType)
+        val fields = List(
+          List("Elevation"), //                              quantitative    meters                       Elevation in meters
+          List("Aspect"), //                                 quantitative    azimuth                      Aspect in degrees azimuth
+          List("Slope"), //                                  quantitative    degrees                      Slope in degrees
+          List("Horizontal_Distance_To_Hydrology"), //       quantitative    meters                       Horz Dist to nearest surface water features
+          List("Vertical_Distance_To_Hydrology"), //         quantitative    meters                       Vert Dist to nearest surface water features
+          List("Horizontal_Distance_To_Roadways"), //        quantitative    meters                       Horz Dist to nearest roadway
+          List("Hillshade_9am"), //                          quantitative    0 to 255 index               Hillshade index at 9am, summer solstice
+          List("Hillshade_Noon"), //                         quantitative    0 to 255 index               Hillshade index at noon, summer soltice
+          List("Hillshade_3pm"), //                          quantitative    0 to 255 index               Hillshade index at 3pm, summer solstice
+          List("Horizontal_Distance_To_Fire_Points"), //    quantitative    meters                       Horz Dist to nearest wildfire ignition points
+          (1 to 4).map(i => s"Wilderness_Area_$i"), // (4 binary columns)     qualitative     0 (absence) or 1 (presence)  Wilderness area designation
+          (1 to 40).map(i => s"Soil_Type_$i"), // (40 binary columns)          qualitative     0 (absence) or 1 (presence)  Soil Type designation
+          List("Cover_Type") // (7 types)                    integer         1 to 7                       Forest Cover Type designation
+        ).flatten
+        val lines = IOUtils.readLines(new FileInputStream("covtype.data.txt"), "UTF8").asScala.toList
+        val dataSet = lines.map(_.trim).filterNot(_.isEmpty).map(x=>{
+          val values = x.split(",").map(Integer.parseInt(_).toInt)
+          val combined = fields.zip(values).toMap
+          new ClassificationTreeItem(combined)
+        }).filter(_.attributes.contains("Cover_Type")).map(_->Random.nextDouble()).sortBy(_._2).map(_._1)
+
+        println(s"Populating tree at ${new Date()}")
+        collection.atomic().sync.setClusterStrategy(new DefaultClassificationStrategy(branchThreshold = Int.MaxValue))
+
+        dataSet.take(items).groupBy(_.attributes("Cover_Type")).foreach(t=>{
+          val (cover_type: Any,stream: Seq[ClassificationTreeItem]) = t
+          stream.grouped(64).map(_.map(item=>item.copy(attributes = item.attributes - "Cover_Type")).toList).foreach(collection.atomic().sync(30.seconds).addAll(cover_type.toString, _))
+        })
+
+
+        println(s"Top-level rule generation at ${new Date()}")
+        awaitTask(collection.atomic().sync.splitTree(new DefaultClassificationStrategy(branchThreshold = 16)), taskTimeout = 30.minutes)
+        println()
+        println(s"Second-level rule generation at ${new Date()}")
+        awaitTask(collection.atomic().sync.splitTree(new DefaultClassificationStrategy(branchThreshold = 4)), taskTimeout = 30.minutes)
+        println()
+
+        println(s"Testing model at ${new Date()}")
+        dataSet.drop(items).take(10).foreach(item => {
+          val testValue = item.copy(attributes = item.attributes - "Cover_Type")
+          val coverType = item.attributes("Cover_Type")
+          val id: STMPtr[ClassificationTreeNode] = collection.atomic().sync(30.seconds).getClusterId(testValue)
+          println(s"$item routed to node "+JacksonValue.simple(id))
+          println(s"Clustered Members: "+JacksonValue.simple(collection.atomic().sync.iterateCluster(id)))
+          println(s"Tree Path: "+JacksonValue.simple(collection.atomic().sync.getClusterPath(id)))
+          val counts: Map[String, Int] = collection.atomic().sync.getClusterCount(id)
+          println(s"Node Counts: "+JacksonValue.simple(counts))
+          println(s"Actual Type: "+coverType)
+          val predictions = counts.mapValues(_.toDouble / counts.values.sum)
+          val prediction = predictions.maxBy(_._2)
+          if(prediction._1 == coverType.toString) {
+            println(s"Correct Prediction: "+prediction._1)
+            println()
+            1
+          } else {
+            println(s"False Prediction: "+prediction._1)
+            println()
+            0
+          }
           println()
         })
 
